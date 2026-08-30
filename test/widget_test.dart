c0eea5cd206eee1348afc86da7ef3f040006642b
verdefra71-oss <verde.fra71@gmail.com>
2026-08-30 15:29:00 +0200

    Add files via upload

---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:preventivi_app/main.dart';
+
+void main() {
+  test('PreventiviApp is available', () {
+    expect(const PreventiviApp(), isA<PreventiviApp>());
+  });
+}

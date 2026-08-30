e83167192defee2b5f2f0718657ea0573af72bb7
verdefra71-oss <verde.fra71@gmail.com>
2026-08-30 20:54:36 +0200

    Delete test directory

--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:preventivi_app/main.dart';
-
-void main() {
-  test('PreventiviApp is available', () {
-    expect(const PreventiviApp(), isA<PreventiviApp>());
-  });
-}

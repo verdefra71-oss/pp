3a7b4e544ac0b5ca25135515337b0e8a04bc187a
verdefra71-oss <verde.fra71@gmail.com>
2026-09-03 17:36:36 +0200

    Delete test directory

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:preventivi_app/main.dart';
-
-void main() {
-  testWidgets('Preventivi app starts', (WidgetTester tester) async {
-    await tester.pumpWidget(const PreventiviApp());
-    expect(find.byType(PreventiviApp), findsOneWidget);
-  });
-}

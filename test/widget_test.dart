38b450335d23374827e0588f90a939191a4713e1
verdefra71-oss <verde.fra71@gmail.com>
2026-09-12 18:04:21 +0200

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:preventivi_app/main.dart';
+import 'package:contabilita_familiare/main.dart';
 
 void main() {
-  testWidgets('Preventivi app starts', (WidgetTester tester) async {
-    await tester.pumpWidget(const PreventiviApp());
-    expect(find.byType(PreventiviApp), findsOneWidget);
+  testWidgets('app starts', (tester) async {
+    final store = FinanceStore();
+    await tester.pumpWidget(FamilyFinanceApp(store: store));
+    expect(find.text('La mia famiglia'), findsOneWidget);
   });
 }
